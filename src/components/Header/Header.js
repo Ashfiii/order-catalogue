e7b007fcd5a8632eac6c77a29108ce189e077280
Ashfiii <alam19231@gmail.com>
2021-05-06 16:31:26 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {Link} from 'react-router-dom';
+import './Header.css';
+import { useSelector } from 'react-redux';
+import jsonData from '../../../public/product.json';
+
+function Header() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const cartArrr= useSelector(
+        (state) => state.carts.cartData
+      );
+      //console.log(cartArrr);
+
+      const filteredSearchTerm = searchTerm.filter()
+
+    return (
+        <div className="header">
+            <div className="header__search">
+                <input 
+                className="header__searchInput" 
+                type="text"
+                placeholder="Search here..."
+                onChange={e=>{setSearchTerm(e.target.value)}}
+                />
+                <SearchIcon className="header__searchIcon"/>
+            </div>
+            <Link to="/checkout">
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">{cartArrr.length}</span>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default Header
